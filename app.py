e4db5027d3f25ec5eb790460f49a9e497d8773ab
Yue <cathywang428@gmail.com>
2022-01-31 20:07:56 -0600

    updated logic to handle multiple surveys

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ app.config['SECRET_KEY']="123"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS']=False
 debug = DebugToolbarExtension(app)
 
-
-response={}
 survey_name = []
 answer_comments={}
+survey_dict={}
 
 @app.route("/")
 def select_survey():
@@ -25,13 +24,14 @@ def start_survey():
 
 @app.route("/questions/<int:index>")
 def survey_questions(index):
-    name = survey_name[0]
+    name = survey_name[-1]
     survey = surveys[name]
     questions = survey.questions
+    response = survey_dict.get(name,{})
     i = len(response)
     if i<len(questions) and index != i:
-        flash(u"You are trying to reach to the wrong question!", "error")
-        flash(u"Redirecting...", "error")
+        flash("You were trying to reach to the wrong question!", "error")
+        flash("Redirecting back...", "error")
         index = i
         return redirect(f"/questions/{index}")
     elif i==len(questions):
@@ -44,17 +44,19 @@ def survey_questions(index):
 
 @app.route("/answer", methods=["POST"])
 def answer():
-    name = survey_name[0]
+    name = survey_name[-1]
     survey = surveys[name]
     questions = survey.questions
     answer = request.form["choices"]
     # import pdb
     # pdb.set_trace()
     # print(response)
+    response = survey_dict.get(name,{})
     index = len(response)
     q = questions[index]
     title = q.question
     response[title]=answer
+    survey_dict[name] = response
     
     if q.allow_text:
         comment = request.form["comments"]
@@ -68,7 +70,8 @@ def answer():
 
 @app.route("/completed")
 def completed():
-    name = survey_name[0]
+    name = survey_name[-1]
     survey = surveys[name]
     questions = survey.questions
+    response = survey_dict.get(name,{})
     return render_template("completed.html", response=response, comments=answer_comments)
